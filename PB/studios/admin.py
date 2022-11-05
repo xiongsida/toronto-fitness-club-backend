f050eb494c02452539e03403bfb0431995a4d220
Sida Xiong <1801197546@qq.com>
2022-11-05 16:37:18 -0400

    add studio related models

--- PB/studios/admin.py
+++ PB/studios/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from studios.models import Studio, StudioImage, Amenity, StudioAmenity
+
+class StudioImageTabularInline(admin.TabularInline):
+    model = StudioImage
+    list_display= ['image','thumbnail']
+    readonly_fields = ['thumbnail']
+    
+class StudioAmenityTabularInline(admin.TabularInline):
+    model=StudioAmenity
+    
+    
+class StudioAdmin(admin.ModelAdmin):
+    inlines = [StudioImageTabularInline, StudioAmenityTabularInline]
+    model = Studio
+    readonly_fields = ['latitude','longitude']
+
 # Register your models here.
+admin.site.register(Studio, StudioAdmin)
+admin.site.register(Amenity)
