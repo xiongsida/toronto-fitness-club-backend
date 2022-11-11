faec2b741e3a66fb2285340c08fce824d2e779f0
Sida Xiong <1801197546@qq.com>
2022-11-11 18:44:19 -0500

    switch class to have a end recur date

--- PB/studios/models/classInstanceException.py
+++ PB/studios/models/classInstanceException.py
@@ -1,9 +1,25 @@
 from django.db import models
+from studios.models.classParent import ClassParent
 from django.db.models import CASCADE
-from studios.models.classEvent import ClassEvent
 
-class ClassInstanceException(models.Model):
-    class_event=models.ForeignKey(to=ClassEvent, on_delete=CASCADE, related_name="exceptions")
-    event_date=models.DateField()
-    is_cancelled=models.BooleanField(default=True)
-    same_for_all_future=models.BooleanField()
+class ClassCanellation(models.Model):
+    class_parent= models.ForeignKey(to=ClassParent, on_delete=CASCADE, related_name="cancellations")
+    cancel_date = models.DateField()
+    cancel_for_all_future = models.BooleanField(default=False)
+    def delete(self):
+        self.save()
+        
+class ClassEdition(models.Model):
+    class_parent= models.ForeignKey(to=ClassParent, on_delete=CASCADE, related_name="edtions")
+    previous_date = models.DateField()
+    description = models.CharField(max_length=512, null=True, blank=True)
+    new_date = models.DateField(null=True,blank=True)
+    start_time = models.TimeField(null=True,blank=True)
+    end_time = models.TimeField(null=True,blank=True)
+    coach = models.CharField(max_length=128,null=True,blank=True)
+    capacity = models.PositiveIntegerField(null=True, blank=True)
+    recurrence_pattern = models.IntegerField(choices=ClassParent.RECURRENCE_WAY,blank=True,null=True) # weekly recurrence
+    recur_end_date = models.DateField(null=True,blank=True)
+    edit_for_all_future = models.BooleanField(default=False)
+    def delete(self):
+        self.save()
--- PB/studios/models/classKeyword.py
+++ PB/studios/models/classKeyword.py
@@ -1,9 +1,9 @@
 from django.db import models
-from studios.models.classEvent import ClassEvent
+from studios.models.classParent import ClassParent
 from django.db.models import CASCADE
 
 class ClassKeyword(models.Model):
-    classparent = models.ForeignKey(to=ClassEvent, on_delete=CASCADE, related_name="keywords")
+    classparent = models.ForeignKey(to=ClassParent, on_delete=CASCADE, related_name="keywords")
     keyword = models.CharField(max_length=128)
     def __str__(self):
         return self.keyword
