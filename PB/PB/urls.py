d492d850554cd177ae4918aef0e82a335199269e
JhongLuo <1999luozhonghao@gmail.com>
2022-11-12 16:38:28 -0500

    fix a merge problem

--- PB/PB/urls.py
+++ PB/PB/urls.py
@@ -25,9 +25,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studios/', include('studios.urls'))
+    path('studios/', include('studios.urls')),
     path("", include('accounts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
