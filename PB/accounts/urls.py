5c550090f4f7b002e6c12e1c86cefb041988b5ec
JhongLuo <1999luozhonghao@gmail.com>
2022-11-12 14:33:23 -0500

    finish auth and users api

---
+++ PB/accounts/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path
+from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
