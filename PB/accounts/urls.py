aefc1f879d0ffd6d74aceaef2884ab22c474b3d5
JhongLuo <1999luozhonghao@gmail.com>
2022-11-12 14:39:52 -0500

    remove redundant code

--- PB/accounts/urls.py
+++ PB/accounts/urls.py
@@ -1,8 +1,6 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
 from accounts import views
-from django.conf import settings
-from django.conf.urls.static import static
 urlpatterns = [
     path('users/', views.UserList.as_view(), name='user_list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
