d9cc1b5419390964e9806632ff3bc9de212e637c
JhongLuo <1999luozhonghao@gmail.com>
2022-11-15 22:26:20 -0500

    Revert "make trailing slash optional"
    This reverts commit 494c501cf8399deb629cb1632aaeebaca4e16652.

--- PB/studios/urls.py
+++ PB/studios/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 from studios.views import StudiosListView, StudioDetailView
 
 app_name = 'studios'
 
 urlpatterns = [
-    re_path(r'^studios/?$', StudiosListView.as_view()),
-    re_path(r'^studios/<int:studio_id>/?$', StudioDetailView.as_view(), name='details'),
+    path('studios', StudiosListView.as_view()),
+    path('studios/<int:studio_id>', StudioDetailView.as_view(), name='details'),
 ]
